73bdf5a32d72468a85d54c664ea6a7ad5d561d11
Mizkus <kirill131c@gmail.com>
2025-07-19 18:30:37 +0700

    add predictor

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from dotenv import dotenv_values
 
 from model.model_client import Model
 from dublgis.dublgis_client import DublGISClient
+from predict_model.model import Predictor
+
 
 def safe_input(prompt):
     try:
@@ -21,6 +23,7 @@ dublgis_key = config['dublgis']
 def chat_loop(level=1):
     print("Чат запущен. Напишите 'по новой' или что-то подобное для сброса. Ctrl+C — выход.\n")
     model_client = Model(config["openai_key"])
+    model_predict = Predictor(config["openai_key"])
     dublgis_client = DublGISClient(config["dublgis"])
     user_id = "default_user"
     city, place, description = dublgis_client.get_random_place_in_city_info()
@@ -33,14 +36,26 @@ def chat_loop(level=1):
             user_input = safe_input("Вы: ").strip()
             if not user_input:
                 continue
-            if user_input.lower() in [
-                "по новой", "заново", "начнём сначала", "сбрось", "reset", "start over"
-            ]:
+            
+            prediction = model_predict.predict(user_input)
+            print(f"SYTEM PREDICTION: {model_predict.predict(user_input)}")
+
+            if prediction == "RESET":
                 model_client.reset(user_id)
                 print("Ассистент: Хорошо, начинаем с начала!\n")
                 city, place, description = dublgis_client.get_random_place_in_city_info()
+                greeting = model_client.init_conv(city, place, description, user_id)
                 print(f"Загадано место: {place}\nОписание: {description}\n")
                 continue
+            
+            if prediction == "GIVE_UP":
+                print(f"Ассистент: Я загадал(а) «{place}». Не расстраивайтесь, попробуем ещё раз!\n")
+                model_client.reset(user_id)
+                city, place, description = dublgis_client.get_random_place_in_city_info()
+                greeting = model_client.init_conv(city, place, description, user_id)
+                print(f"Загадано место: {place}\nОписание: {description}\n")
+                continue
+
             reply = model_client.ask(user_id, user_input)
             print(f"Ассистент: {reply}\n")
     except KeyboardInterrupt:
