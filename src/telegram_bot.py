d497fd6f0fdb0e05197473b9404164494ec2aae5
Nikita Koshelev <nikita.koshelev@shkulev.ru>
2025-07-20 13:04:18 +0700

    Fix links in answer

--- src/telegram_bot.py
+++ src/telegram_bot.py
@@ -1,5 +1,5 @@
 import os
-import re
+import html
 import logging
 from dotenv import dotenv_values
 from telegram import Update
@@ -48,10 +48,6 @@ def get_user_logger(user_id: str) -> logging.Logger:
     return logger
 
 
-def escape_markdown(text: str) -> str:
-    return re.sub(r"([_*\[\]()~`>#+\-=|{}.!\\])", r"\\\1", text)
-
-
 def new_round(user_id: str, context: ContextTypes.DEFAULT_TYPE) -> str:
     city, place, description, lat, lon = dublgis_client.get_random_place_in_city_info()
     context.user_data.update(
@@ -82,9 +78,8 @@ async def reset_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     user_logger.info("CMD: /reset")
     reply = model_client.reset(user_id)
-    escaped_reply = escape_markdown(reply)
     model_predict.reset(user_id)
-    user_logger.info(f"MODEL RESET REPLY: {escaped_reply}")
+    user_logger.info(f"MODEL RESET REPLY: {reply}")
 
     await update.message.reply_text("Диалог перезапущен.")
     greeting = new_round(user_id, context)
@@ -102,8 +97,7 @@ async def handle_text(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
     user_logger.info(f"USER: {user_input}")
 
     reply = model_client.ask(user_id, user_input)
-    escaped_reply = escape_markdown(reply)
-    model_predict.remember_assistant(user_id, escaped_reply)
+    model_predict.remember_assistant(user_id, reply)
 
     prediction = model_predict.predict(user_id, user_input)
     user_logger.info(f"PREDICTOR: {prediction}")
@@ -115,17 +109,21 @@ async def handle_text(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
         user_logger.info(f"place: {place}")
 
         reply = model_client.bid_farewell(user_id)
-        escaped_reply = escape_markdown(reply)
         model_predict.reset(user_id)
 
-        user_logger.info(f"MODEL RESET REPLY: {escaped_reply}")
+        user_logger.info(f"MODEL RESET REPLY: {reply}")
         image_url = dublgis_client.get_place_image_url()
+        link = html.escape(dublgis_client.get_browser_link(dublgis_client.current_place_id), quote=True)
+        reply_with_link = (
+            f"{reply}\n<a href=\"{link}\">Ссылка на место в 2ГИС</a>"
+        )
+
         if image_url:
             await update.message.reply_photo(
-                photo=image_url, caption=escaped_reply, parse_mode="MarkdownV2"
+                photo=image_url, caption=reply_with_link, parse_mode="HTML"
             )
         else:
-            await update.message.reply_text(escaped_reply, parse_mode="MarkdownV2")
+            await update.message.reply_text(reply_with_link, parse_mode="HTML")
 
         if place and lon and lat:
             nearby_place = dublgis_client.get_nearby_places(lat, lon, user_logger)[0]
@@ -140,15 +138,14 @@ async def handle_text(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
             img_url = nearby_place["image_url"]
             place_url = nearby_place["object_link"]
             reply = model_client.recommmend(user_id, result_to_model)
-            reply += f"\n Вот ссылка {place_url}, если тебе интересно."
-            escaped_reply = escape_markdown(reply)
+            reply += f'\nВот <a href="{place_url}">ссылка на это место</a>, если тебе интересно.'
 
             if img_url:
                 await update.message.reply_photo(
-                    photo=img_url, caption=escaped_reply, parse_mode="MarkdownV2"
+                    photo=img_url, caption=reply, parse_mode="HTML"
                 )
             else:
-                await update.message.reply_text(escaped_reply, parse_mode="MarkdownV2")
+                await update.message.reply_text(reply, parse_mode="HTML")
         model_client.reset(user_id)
         greeting = new_round(user_id, context)
         await update.message.reply_text(greeting)
@@ -171,27 +168,29 @@ async def handle_text(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
             )
             if image_url:
                 await update.message.reply_photo(
-                    photo=image_url, caption=caption, parse_mode="MarkdownV2"
+                    photo=image_url, caption=caption, parse_mode="HTML"
                 )
             else:
-                await update.message.reply_text(caption, parse_mode="MarkdownV2")
+                await update.message.reply_text(caption, parse_mode="HTML")
     if remaining == 0:
         reply = model_client.reset(user_id)
         model_predict.reset(user_id)
-        escaped_reply = escape_markdown(reply)
 
-        await update.message.reply_text(
-            "Попытки исчерпаны, и не удалось получить подсказку.\n"
-            "Вот правильный ответ:\n\n" + escaped_reply,
-            parse_mode="MarkdownV2",
+        link = html.escape(dublgis_client.get_browser_link(dublgis_client.current_place_id), quote=True)
+        reply_text = (
+            "Попытки исчерпаны, и не удалось получить подсказку.<br>"
+            f"Вот правильный ответ:<br><br>{reply}<br>"
+            f'<a href="{link}">Ссылка на место в 2ГИС</a>'
         )
+
+        await update.message.reply_text(reply_text, parse_mode="HTML")
+
         greeting = new_round(user_id, context)
         await update.message.reply_text(greeting)
         return
 
     user_logger.info(f"MODEL: {reply}")
-    escaped_reply = escape_markdown(reply)
-    await update.message.reply_text(escaped_reply)
+    await update.message.reply_text(reply)
 
 
 def main() -> None:
